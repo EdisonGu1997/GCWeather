b59dba1bef85f086d110d5ee7305787e13b2e6ae
EdisonGu <edisongu1997@outlook.com>
2017-11-23 20:27:58 +0800

    create project and init

---
+++ app/src/main/java/com/gsoft/gcweather/app/MyApplication.java
@@ -0,0 +1,11 @@
+package com.gsoft.gcweather.app;
+
+import android.app.Application;
+
+/**
+ * Created by edison on 2017/11/23 0023.
+ */
+
+public class MyApplication extends Application {
+    private static 
+}
